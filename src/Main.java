eccd046c209fc358951df5e5b5d31750b1f2919d
Bipro <biprobarai18@gmail.com>
2021-08-18 21:09:38 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle1 = new Vehicle();
+        vehicle1.setColor("Red");
+        vehicle1.setDoors(6);
+        vehicle1.setWheels(4);
+        System.out.println(vehicle1);
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,42 @@
+public class Vehicle {
+    private int doors;
+    private int wheels;
+    private String color;
+    private String name;
+    public String toString(){
+        String description = "This vehicle has "+doors+ "\n Wheels is :"+wheels+"\nand color is :"+color;
+        return description;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
